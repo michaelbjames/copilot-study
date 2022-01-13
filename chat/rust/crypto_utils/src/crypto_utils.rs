8c9a7072e8f5b8a0e351df405a32257316c72682
John Renner <john@jrenner.net>
2022-01-12 20:51:27 -0800

    Make more idiomatic

--- chat/rust/crypto_utils/src/crypto_utils.rs
+++ chat/rust/crypto_utils/src/crypto_utils.rs
@@ -1,29 +1,19 @@
-use std::collections::BTreeMap;
-
-pub use bp256::r1::BrainpoolP256r1;
-use num::{BigInt, BigUint};
-extern crate rand;
+use num::{BigUint};
 use rand::Rng;
 use openssl::symm::{Cipher, Crypter, Mode};
-use byteorder::WriteBytesExt;
-use byteorder::BigEndian;
-use num::ToPrimitive;
-use rustc_serialize::hex::ToHex;
-use serde::{Deserialize, Serialize};
-use serde_json::json;
 
 pub trait Crypto {
     fn new() -> Self;
     fn encrypt(&self, plaintext: &[u8]) -> Vec<u8>;
     fn decrypt(&self, ciphertext: &[u8]) -> Vec<u8>;
-    fn gen_priv_key(&self) -> BigInt;
-    fn gen_pub_key(&self, priv_key: &mut BigInt) -> BigInt;
-    fn compute_shared_secret(&self, priv_key: &mut BigInt, other_pub_key: BigInt) -> BigInt;
-    fn handshake(&mut self, priv_key: &mut BigInt, other_pub_key: BigInt) -> ();
-    fn serialize(&self, pub_key: &BigInt) -> Vec<u8>;
-    fn deserialize(&self, pub_key: &[u8]) -> BigInt;
-    fn init_key(&mut self, key: Vec<u8>) -> ();
-    fn generate_keys(&self) -> (BigInt, Vec<u8>);
+    fn gen_priv_key(&self) -> BigUint;
+    fn gen_pub_key(&self, priv_key: &BigUint) -> BigUint;
+    fn compute_shared_secret(&self, priv_key: &BigUint, other_pub_key: &BigUint) -> BigUint;
+    fn handshake(&mut self, priv_key: &BigUint, other_pub_key: &BigUint);
+    fn serialize(&self, pub_key: &BigUint) -> Vec<u8>;
+    fn deserialize(&self, pub_key: &[u8]) -> BigUint;
+    fn init_key(&mut self, key: Vec<u8>);
+    fn generate_keys(&self) -> (BigUint, Vec<u8>);
 }
 
 pub struct PrimeDiffieHellman {
@@ -31,10 +21,6 @@ pub struct PrimeDiffieHellman {
     g: usize,
     cipher: Cipher,
     key: Vec<u8>
-}
-#[derive(Serialize, Deserialize)]
-pub struct KeyData {
-    key_val: u16
 }
 
 impl Crypto for PrimeDiffieHellman {
@@ -45,22 +31,6 @@ impl Crypto for PrimeDiffieHellman {
             p: 997,
             g: 2,
         }
-    }
-
-    fn gen_priv_key(&self) -> BigInt {
-        let mut rng = rand::thread_rng();
-        let priv_key = rng.gen_range(1..(self.p - 1));
-        return BigInt::from(priv_key);
-    }
-
-    fn gen_pub_key(&self, priv_key: &mut BigInt) -> BigInt {
-        let pub_key = BigInt::modpow(&BigInt::from(self.g), &priv_key, &BigInt::from(self.p));
-        return pub_key;
-    }
-
-    fn compute_shared_secret(&self, priv_key: &mut BigInt, other_pub_key: BigInt) -> BigInt {
-        let secret = BigInt::modpow(&other_pub_key, &priv_key, &BigInt::from(self.p));
-        return secret;
     }
 
     fn encrypt(&self, plaintext: &[u8]) -> Vec<u8> {
@@ -77,9 +47,9 @@ impl Crypto for PrimeDiffieHellman {
 
     fn decrypt(&self, data: &[u8]) -> Vec<u8> {
         let mut decrypted = Crypter::new(self.cipher, Mode::Decrypt, &self.key, None).unwrap();
-        let mut output = vec![0 as u8; data.len() + self.cipher.block_size()];
+        let mut output = vec![0_u8; data.len() + self.cipher.block_size()];
     
-        let decrypted_result = decrypted.update(&data, &mut output);
+        let decrypted_result = decrypted.update(data, &mut output);
     
         match decrypted_result {
             Ok(_) => output,
@@ -87,45 +57,43 @@ impl Crypto for PrimeDiffieHellman {
         }
     }
 
-    fn generate_keys(&self) -> (BigInt, Vec<u8>) {
-        let mut priv_key = self.gen_priv_key();
-        let pub_key = self.gen_pub_key(&mut priv_key);
-        let pubkey_bytes = self.serialize(&pub_key);
-        return (priv_key, pubkey_bytes.to_vec());
+    fn gen_priv_key(&self) -> BigUint {
+        let mut rng = rand::thread_rng();
+        let priv_key = rng.gen_range(1..(self.p - 1));
+        BigUint::from(priv_key)
     }
 
-    fn handshake(&mut self, priv_key: &mut BigInt, other_pub_key: BigInt) {
+    fn gen_pub_key(&self, priv_key: &BigUint) -> BigUint {
+        BigUint::from(self.g).modpow(priv_key, &self.p.into())
+    }
+
+    fn compute_shared_secret(&self, priv_key: &BigUint, other_pub_key: &BigUint) -> BigUint {
+        other_pub_key.modpow(priv_key, &BigUint::from(self.p))
+    }
+
+    fn handshake(&mut self, priv_key: &BigUint, other_pub_key: &BigUint) {
         let shared_secret = self.compute_shared_secret(priv_key, other_pub_key);
         println!("Shared secret: {}", shared_secret);
-        let mut wtr = Vec::new();
-            wtr.write_u16::<BigEndian>(ToPrimitive::to_u16(&shared_secret).unwrap()).unwrap();
-            while wtr.len() != 16 {
-                wtr.push(0);
-            }
-        self.init_key(wtr[..].to_vec());
-        return;
+        let key = shared_secret.to_bytes_be();
+        self.init_key(key)
+    }
+
+    fn serialize(&self, pub_key: &BigUint) -> Vec<u8> {
+        pub_key.to_bytes_be()
+    }
+
+    fn deserialize(&self, pub_key: &[u8]) -> BigUint {
+        BigUint::from_bytes_be(pub_key)
     }
 
     fn init_key(&mut self, key: Vec<u8>) {
         self.key = key;
     }
 
-    fn serialize(&self, pub_key: &BigInt) -> Vec<u8> {
-        let key_data = KeyData {
-            key_val: ToPrimitive::to_u16(pub_key).unwrap(),
-        };
-        let key_json_str = serde_json::to_string(&key_data).unwrap();
-        return key_json_str.as_bytes().to_vec();
+    fn generate_keys(&self) -> (BigUint, Vec<u8>) {
+        let priv_key = self.gen_priv_key();
+        let pub_key = self.gen_pub_key(&priv_key);
+        let pubkey_bytes = self.serialize(&pub_key);
+        (priv_key, pubkey_bytes.to_vec())
     }
-
-    fn deserialize(&self, pub_key: &[u8]) -> BigInt {
-        let key_json_str = String::from_utf8(pub_key.to_vec());
-        let key_json_str_trimmed = key_json_str.unwrap().trim_matches(char::from(0)).to_string();
-        let key_data: KeyData = serde_json::from_str(&key_json_str_trimmed).unwrap();
-        let key_value = key_data.key_val;
-        return BigInt::from(key_value);
-    }
-}
-
-pub fn main() {
 }
