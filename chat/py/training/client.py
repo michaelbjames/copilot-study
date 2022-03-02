48e80c5b7b7b5d7d5babb284290d2398a6c51d26
Michael James <hello@michaelbjames.com>
2022-03-02 12:42:15 -0800

    python training includes a error to show if their setup includes simple warnings

--- chat/py/training/client.py
+++ chat/py/training/client.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 import sys
+
+
+
+"""
+Does your usual setup include some python checking plugins?
+
+They should find an error in the following code:
+"""
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        # fib_dne is not defined, and should get some kind of squiggle or mark
+        return fib_dne(n - 1) + fib(n - 2)
