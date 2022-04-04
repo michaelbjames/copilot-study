39dbdbee3db056fcc733baaacf81e3e42aa4458e
Shraddha Barke <sbarke@eng.ucsd.edu>
2022-04-03 23:02:17 -0700

    adding rust version

--- task5/python/advent2.py
+++
@@ -1,6 +0,0 @@
-import os
-
-if __name__ == "__main__":
-    dir = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(dir, "input-day2.txt")) as file:
-        data = file.read().splitlines()
